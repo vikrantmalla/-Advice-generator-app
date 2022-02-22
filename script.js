20995c755a159b5d66209b37a982e236d91476c9
Vikrant Malla <vikrantmalla999@gmail.com>
2022-02-22 21:20:07 +0545

    optimized code

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const quote_id = document.getElementById('quote_id');
-const quote_text = document.getElementById('quote_text')
+const quote_id = document.querySelector('#quote_id');
+const quote_text = document.querySelector('#quote_text')
 const btn = document.getElementById('next')
 async function fetchAdvice() {
     let url = 'https://api.adviceslip.com/advice';
     btn.classList.add("loading")
     const data = await fetch(url);
     const json = await data.json();
-    quote_id.textContent = json.slip.id
-    quote_text.textContent = json.slip.advice
+    quote_id.innerHTML = json.slip.id
+    quote_text.innerHTML = `"${json.slip.advice}"`
     btn.classList.remove("loading")
 }
 
 
-
-
 document.addEventListener('DOMContentLoaded', async() => {
     try {
-        fetchAdvice()
         btn.addEventListener("click", fetchAdvice)
     } catch (e) {
         console.log(e);
